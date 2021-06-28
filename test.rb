cb4cbe89cfeab11a5b834ff6fabe374383813c3a
Jordan Reed <jordanreed03@gmail.com>
2021-06-28 19:12:32 +0000

    Automatically backed up by Learn

---
+++ test.rb
@@ -0,0 +1,6 @@
+answer = "flamboyance"
+puts "A group of flamingos is called a #{answer}."
+
+def greet_user 
+    puts say hello 
+end
---
+++ test2.rb
@@ -0,0 +1,8 @@
+
+def weiner(x) 
+  x.times do
+            puts "8======D"
+    end
+  end
+    
+weiner(5)
